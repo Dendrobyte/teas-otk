ca3ddc84e1e1a3dadbdfc3df7a0afe985dda8ce6
Dendrobyte <markbacon78@gmail.com>
2026-02-17 08:20:51 -0800

    Sorta tweaked the textures, camera moved, camera follows player in controller

--- assets/asset_plane_base.gd
+++ assets/asset_plane_base.gd
@@ -1,13 +1,12 @@
 extends Node3D
 
-@export var plane_texture_img: Texture2D = preload("res://assets/asset_plane_default_image.png")
+@export var plane_texture_img: Texture2D;
 
 func _ready():
 	# Plane dimensions should match the image (our source of truth)
 	var img_size = plane_texture_img.get_size()
 	var aspect = img_size.x / img_size.y
 	$PlaneMesh.mesh.size = Vector2(aspect*2, 2.0)
-	print(img_size.x, img_size.y)
 	
 	# Ensure our pivot point is set up as intended, programmatically to verify
 	# TODO: If we're in the brewing phase, set the rotation to full 90
