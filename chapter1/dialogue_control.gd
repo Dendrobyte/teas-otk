a18e1d80fec2b25e002a4325c21e737def3af40a
Dendrobyte <markbacon78@gmail.com>
2026-03-06 08:29:49 -0800

    Dialogue in control node

--- chapter1/dialogue_control.gd
+++ chapter1/dialogue_control.gd
@@ -8,18 +8,26 @@ var npc_dialogue_map = {}
 # As this grows bigger, move things into child scripts. Otherwise, we can just keep it in here for now
 func _ready():
 	hide()
+	load_dialogue()
 	connect_to_npc_signals()
+
+# TODO: THIS IS A MAP OF ALL DIALOGUE FOR CHAPTER ONE
+# MOVE THIS TO A FILE!!!!
+func load_dialogue():
+	npc_dialogue_map = {
+		"NPC1": "Ey kid... boy, lot of us sure are lookin' worse for wear. // Military sure is convinced they need to give everything to the soldiers but they're forgettin' about their people... *grumble grumble*",
+		"NPC2": "JERRY! Stop talkin' to randos out on the street. Help me pack up the crates, we need to set up early today. // _She tosses you an apple_. Be on your way, kid.",
+		"NPC3": "Gah... the lines haven't been this long in years!! // That damned population increase...",
+		"NPC4": "<Conversation with NPC5>",
+		"NPC5": "<Conversation with NPC4>",
+		"NPC6": "Permit please!", # "Papers, please!" as a small ode to that game lol
+	} 
 
 func connect_to_npc_signals():
 	# The NPC parent MUST be on the same level as our dialogue control node
 	var npc_parent_node = get_parent().get_node("NPCs")
 	for npc_node in npc_parent_node.get_children():
 		var npc: NPCBase = npc_node as NPCBase
-		# We have all dialogue loaded up here
-		# TODO: Move this global state map to the control mode, i.e. write a "load dialogue" function here
-		# This doesn't need to exist on the global state
-		# And thus we can remove this line since it's just the same map
-		npc_dialogue_map[npc.name] = GlobalState.DIALOGUE_FILE_PATH[npc.name]
 
 		npc.npc_collision_enter.connect(_on_character_enters_npc_area)
 		npc.npc_collision_leave.connect(_on_character_leaves_npc_area)
@@ -33,8 +41,6 @@ func _on_character_enters_npc_area(body: Node3D):
 func _on_character_leaves_npc_area(body: Node3D):
 	var npc_name = body.name
 	print("NPC ", npc_name, " button hides on exit")
-
-# When an NPC emits a collision signal on leave, we hide the interact button
 
 # When the interact button is hit and it's visible, show the NPC dialogue that emitted the last entry signal
 # TODO: Freeze all other actions -> look into the unhandled_input flow chart again, I think that may be our answer
--- global_state.gd
+++ global_state.gd
@@ -16,16 +16,6 @@ var ROTATION_ANGLE = {
 
 const BASE_TILE_SIZE = 128
 
-# THIS IS A MAP OF ALL DIALOGUE FOR CHAPTER ONE
-# MOVE THIS TO A FILE!!!!
-const DIALOGUE_FILE_PATH = {
-	"NPC1": "Ey kid... boy, lot of us sure are lookin' worse for wear. // Military sure is convinced they need to give everything to the soldiers but they're forgettin' about their people... *grumble grumble*",
-	"NPC2": "JERRY! Stop talkin' to randos out on the street. Help me pack up the crates, we need to set up early today. // _She tosses you an apple_. Be on your way, kid.",
-	"NPC3": "Gah... the lines haven't been this long in years!! // That damned population increase...",
-	"NPC4": "<Conversation with NPC5>",
-	"NPC5": "<Conversation with NPC4>",
-	"NPC6": "Permit please!", # "Papers, please!" as a small ode to that game lol
-}
 
 # "Now whenever we run any scene in the project, this script will always be loaded."
 # But does that mean it also maintains its state...?
