43aa4ea6b28005ec9d3efef7d21e70b23fd543ac
Dendrobyte <markbacon78@gmail.com>
2026-03-10 09:06:57 -0700

    YarnSpinner work continues

--- chapter1/dialogue_control.gd
+++ chapter1/dialogue_control.gd
@@ -21,11 +21,32 @@ var old_man: NPCBase = null
 
 ## End of NPC stuff
 
+## YarnSpinner setup
+# Just hoping that we can have the YarnDialogueRunner as the child of this control and be fine, thus we can create a scene out of the DialogueControl
+@onready var dialogue_runner := $YarnDialogueRunner
+@onready var line_presenter := $CanvasLayer/LinePresenter
+@onready var options_presenter := $CanvasLayer/OptionsPresenter
+
+## End of YarnSpinner setup
+
+# Separate show/hide functions in case I add stuff to show/hide that isn't just the canvas layer
+func show_dialogue():
+	$CanvasLayer.show()
+
+func hide_dialogue():
+	$CanvasLayer.hide()
+
 # General parent dialogue control
 # As this grows bigger, move things into child scripts. Otherwise, we can just keep it in here for now
 func _ready():
-	# hide()
-	load_dialogue()
+	## YarnSpinner readying setup
+	dialogue_runner.add_presenter(line_presenter)
+	dialogue_runner.add_presenter(options_presenter)
+	# I wish I had elixir lambda here
+	dialogue_runner.variable_storage.variable_changed.connect(update_flag)
+
+	# Move to show/hide and dialogue loading
+	hide_dialogue()
 	connect_to_npc_signals()
 
 	# Set up the interact button
@@ -40,29 +61,8 @@ func _input(event):
 	#       We can just "handle" all input here by sinking it into an empty return
 	# TODO: If this dialogue triggers an event change (like talking to the old man), reload their dialogue
 	if event.is_action_pressed("interact") and can_interact:
-		print(curr_npc + " says " + curr_dialogue)
-		check_npc_event(curr_npc)
-
-
-# TODO: THIS IS A MAP OF ALL DIALOGUE FOR CHAPTER ONE
-# MOVE THIS TO A FILE!!!!
-# Note: Just to store notes on dialogue systems, the "dialogue" should always be a blob
-# That way we can use the npc name to retrieve it but the NPC "actual name" will be in the blob to then show in a diff field
-# Such as when we interact, it's curr_dialogue.name + ": " + curr_dialogue.text_start or something
-func load_dialogue():
-	npc_dialogue_map = {
-		"NPC1": "Ey kid... boy, lot of us sure are lookin' worse for wear. // Military sure is convinced they need to give everything to the soldiers but they're forgettin' about their people... *grumble grumble*",
-		"NPC2": "JERRY! Stop talkin' to randos out on the street. Help me pack up the crates, we need to set up early today. // _She tosses you an apple_. Be on your way, kid.",
-		"NPC3": "Gah... the lines haven't been this long in years!! // That damned population increase...",
-		"NPC4": "<Conversation with NPC5>",
-		"NPC5": "<Conversation with NPC4>",
-		"CitadelGuard":
-			{
-				"flag_talked_to_old_man_false": "Permit please!", # "Papers, please!" as a small ode to that game lol
-				"flag_talked_to_old_man_true": "Welcome in, you two",
-			},
-		"OldMan": "Ah, hello there young fellow!",
-	}
+		dialogue_runner.start_dialogue(curr_npc)
+		show_dialogue()
 
 func connect_to_npc_signals():
 	# The NPC parent MUST be on the same level as our dialogue control node
@@ -73,6 +73,8 @@ func connect_to_npc_signals():
 		npc.npc_collision_enter.connect(_on_character_enters_npc_area)
 		npc.npc_collision_leave.connect(_on_character_leaves_npc_area)
 
+		# TODO: Is there a better way to register this kind of thing?
+		# How can we move it into the EVENT TRIGGERING block so we can load these unique events in different overworld scenes?
 		if npc.name == "OldMan":
 			old_man = npc
 			old_man.hide() # This doesn't hide the hitbox
@@ -86,26 +88,13 @@ func _on_character_enters_npc_area(body: Node3D):
 	interact_button.position = location
 	interact_button.show()
 	can_interact = true
-	curr_dialogue = npc_dialogue_map[npc_name]
-
-	## EVENT TRIGGERING ##
-	# This is the place where I want something that could potentially be more dynamic...
-	# Like checking this flag on every character here- is that a good place for it?
-	# Or is it better to update a new map of dialogue with "what should be said given the current state"
-	# when the state changes?
-	# I don't like that this is so conditional here, so tracking it "elsewhere" would make it more
-	# readable- i.e. the above line npc_dialogue_map[npc_name] would be the only line we need
-	if npc_name == "CitadelGuard":
-		if flag_talked_to_old_man == false:
-			curr_dialogue = curr_dialogue["flag_talked_to_old_man_false"]
-		else:
-			curr_dialogue = curr_dialogue["flag_talked_to_old_man_true"]
-
 	curr_npc = npc_name
 
 func _on_character_leaves_npc_area(_body: Node3D):
 	interact_button.hide()
 	can_interact = false
+	hide_dialogue()
+
 
 ## EVENT TRIGGERING ##
 # I don't like that this is in the dialogue control. It has nothing to do with dialogue. But I think
@@ -113,12 +102,40 @@ func _on_character_leaves_npc_area(_body: Node3D):
 # global state. Then we save that global state with all those independent fields
 # And somehow each scene will have potential events and such to trigger, as opposed to all of them
 # being in one huge file
+# UPDATE: I think... this is OK in dialogue control for now actually.
+# But what we need to do is, as this evolves into new scenes, load each overworld level individually
+# And each overworld level will have its own set of flags, etc. which we load in
+# whenever the dialogue control is loaded in
+# So everything below here, effectively, will be moved out to a new custom node/script
+# Except maybe the update_flag function or anything called from yarn spinner listeners
+# That should be the connect YS -> Dialogue Control -> Story State
 
-var flag_checked_for_permit = false
-var flag_talked_to_old_man = false
-func check_npc_event(npc_name):
-	if npc_name == "CitadelGuard":
-		flag_checked_for_permit = true
-		old_man.show()
-	if npc_name == "OldMan":
-		flag_talked_to_old_man = true
+# Holds on to a flag value and the function to call when it changes
+var FLAGS = {
+	"checked_for_permit": { # Talk to CitadelGuard
+		"value": false,
+		"function": on_checked_for_permit,
+	},
+	"talked_to_old_man": { # Talk to CitadelGuard
+		"value": false,
+		"function": on_talked_to_old_man,
+	}
+}
+
+# Connected to the variable_storage variable_changes signal
+# TODO: Update some map of flags in this file
+func update_flag(var_name, value):
+	print("Updating ", var_name, " to ", value)
+
+# All the functions executed on flag change to keep the map somewhat readable
+func on_checked_for_permit():
+	print("Permit checked!")
+	# YarnSpinner flag updates should take it from here, as below
+	# TODO: Spawn in the old man
+
+func on_talked_to_old_man():
+	print("Talked to old man!")
+	# Since our YS flag update function should update the var, I don't think we need to do anything for that
+	# TODO: Ensure player can't interact (talk again) to old man, e.g. remove bounding box?
+
+# End of flag execution functions
