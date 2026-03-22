6947ad31e2d1534e3687d13919da732f35188334
Dendrobyte <markbacon78@gmail.com>
2026-03-22 09:20:19 -0700

    Cast ray ahead of player

--- chapter2/char_movement.gd
+++ chapter2/char_movement.gd
@@ -5,6 +5,7 @@ extends CharacterBody3D
 @export var speed = 10
 # TODO: Do this programatically instead of in the editor? Depends on how I spawn in the scene I guess
 @export var dialogue_runner: Node
+@export var offset_const = .5
 
 var target_velocity = Vector3.ZERO
 var is_in_dialogue = false
@@ -23,39 +24,49 @@ func _ready():
 		dialogue_runner.dialogue_completed.connect(func(): is_in_dialogue = false)
 
 var gravity = 8
-func _physics_process(delta):
+func _physics_process(_delta):
 	if is_in_dialogue:
 		return
 
 	var direction = Vector3.ZERO
 
-	if not is_on_floor():
-		target_velocity.y -= gravity * delta
+	# I don't think I need falling logic yet?
+	# if not is_on_floor():
+	# 	target_velocity.y -= gravity * delta
 
+	# We want to cast the floor collision ray where they're going, not where they are, to avoid getting stuck
+	var ray_offset = {
+		x = 0,
+		z = 0,
+	}
 
 	# TODO: Support both movements to go diagonally
 	if Input.is_action_pressed("move_up"):
 		direction.z -= 1
+		ray_offset.z = offset_const*-1
 	if Input.is_action_pressed("move_down"):
 		direction.z += 1
+		ray_offset.z = offset_const # Moving "down" is positive in the Z direction, toward the camera
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+		ray_offset.x = offset_const
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
+		ray_offset.x = offset_const*-1
 
 	# Snap y level to ground mesh
 	var space_state = get_world_3d().direct_space_state
-	var ray_start = Vector3(global_position.x, global_position.y+5, global_position.z) # Start above the player?
-	var ray_end = Vector3(global_position.x, global_position.y-10, global_position.z) # I don't think we need to go too far down?
+	var ray_start = Vector3(global_position.x+ray_offset.x, global_position.y+5, global_position.z+ray_offset.z) # Start above the player?
+	var ray_end = Vector3(global_position.x+ray_offset.x, global_position.y-10, global_position.z+ray_offset.z) # I don't think we need to go too far down?
 	var query = PhysicsRayQueryParameters3D.create(ray_start, ray_end)
 	query.exclude = [self]
 	var result = space_state.intersect_ray(query)
 	# If no result, player is attempting to walk off something
 	# TODO: Mess with vector length, potentially "float" players down if not high up
 	if result:
-		global_position.y = result.position.y + 0.5
+		global_position.y = result.position.y
 
-	if direction != Vector3.ZERO:
+	if direction != Vector3.ZERO and result:
 		direction = direction.normalized()
 		# TODO: Update the sprite shown here based on direction
 		target_velocity.x = direction.x * speed
