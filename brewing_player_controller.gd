444f94c41fd29ad18aa10e6fa5ed84fe7757845d
Dendrobyte <markbacon78@gmail.com>
2026-04-10 09:53:56 -0700

    Teabag model, cup can be picked up

--- brewing_player_controller.gd
+++ brewing_player_controller.gd
@@ -5,29 +5,20 @@ extends Node3D
 @export var pitch_min: float = -30.0  # degrees down
 @export var pitch_max: float = 25.0   # degrees up
 @export var ray_length = 100
+@onready var debug_text_label = $Control/DebugText
 
+# Assets to preload
+@onready var teabag_model = preload("res://assets/models/teabag.glb")
+
+# Camera & character movement
+@onready var cam = $Camera3D
 var yaw: float = 0.0
 var pitch: float = 0.0
 
-var debug_cyl = null
+# Some text as a temporary visual indicator of where I am and what's updating where
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-## Temporary order clicking
-# Figuring out a proper system for this is TBD, I just want to get a full loop and then come in and redo everything
-var next_item_idx = 0
-var item_seq = ["Pot", "Burner", "Cup"]
-
-func proceed_to_next(clicked_item_name):
-	if next_item_idx == len(item_seq):
-		print("You are done!")
-	else:
-		if clicked_item_name.contains(item_seq[next_item_idx]):
-			print("Step completed!")
-			next_item_idx += 1
-		else:
-			print("Wrong step")
-## End of that stuff
+	debug_text_label.text = "Entered scene"
 
 # NOTE: Potential use case for _unhandled_input?
 func _input(event):
@@ -40,16 +31,16 @@ func _input(event):
 	if event.is_action_pressed("escape"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if event.is_action_pressed("click"):
+		# NOTE: Just the structure of this item
 		## { "position": (-4.876412, 3.20117, 5.886989), "normal": (1.0, 0.0, 0.0), "face_index": -1, "collider_id": 30400316950, "collider": Pot:<StaticBody3D#30400316950>, "shape": 0, "rid": RID(631360192512) }
 		var collided_obj = check_raycast_collision()
 		if !collided_obj.is_empty():
 			# TODO: Ideally the objects themselves only have visual updates, e.g. pot.start_boiling, and everything else handled otherwise
 			# 		or through property access, e.g. cup.tea_type
 			# TODO: Emit a signal if we update the inventory with this selection or something
-			proceed_to_next(collided_obj.collider.name)
+			trigger_item_interaction(collided_obj.collider)
 
 func check_raycast_collision():
-	var cam = $Camera3D
 	var origin = cam.project_ray_origin($Control.cursor_pos)
 	var end = origin + cam.project_ray_normal($Control.cursor_pos) * ray_length
 	var query = PhysicsRayQueryParameters3D.create(origin, end)
@@ -58,3 +49,50 @@ func check_raycast_collision():
 	var space_state = get_world_3d().direct_space_state
 	var result = space_state.intersect_ray(query)
 	return result
+
+# Not all of them use the passed in node, but it's good to have
+var item_interact_funcs = {
+	"TeaInv": shelf_interaction,
+	"TeaCup": cup_interaction,
+}
+func trigger_item_interaction(item_node_collider):
+	var item_node = item_node_collider.get_parent()
+	var item_name = item_node.name
+	print("Triggering interaction for: ", item_name)
+	var min_name = item_name.rstrip("0123456789")
+	# TODO: This passes the collider, so get the parent
+	item_interact_funcs[min_name].call(item_node)
+
+
+
+## ITEM FUNCTIONS ##
+# Some day, maybe adding state to each of these items
+# But since I'm not worrying about animations or anything now, we're good
+
+var has_teabag = false
+var teabag = null
+func shelf_interaction(_tea_inv_node):
+	if not has_teabag:
+		teabag = teabag_model.instantiate()
+		teabag.scale = Vector3(teabag.scale.x*.5, teabag.scale.y*.5, teabag.scale.z*.5)
+		add_child(teabag) # TODO: Add to a different child? But I want it to track there... So maybe I do keep it on player?
+		has_teabag = true
+		teabag.global_position = get_hold_location_pos()
+		debug_text_label.text = "Teabag picked up"
+
+# TODO: Place teabag in cup, then pick up cup
+func cup_interaction(cup_node):
+	if has_teabag:
+		print("Teabag had! Pickup cup")
+		teabag.queue_free()
+		has_teabag = false
+		cup_node.global_position = get_hold_location_pos()
+		# TODO: Should probably delete cup, then spawn a new one actually, so we can add as child here
+		debug_text_label.text = "Cup picked up with teabag inside"
+		# TODO: Can put cup down somewhere to pour water into
+
+# We'll constantly use the ray approach to put an item at a position in front of the player
+func get_hold_location_pos():
+	var origin = cam.project_ray_origin($Control.cursor_pos)
+	var end = origin + cam.project_ray_normal($Control.cursor_pos) * 4
+	return end
