2d8a49f347c165e151194f4adffc2d34c6c12340
Dendrobyte <markbacon78@gmail.com>
2026-04-12 09:08:33 -0700

    Cup placement works for now

--- brewing_player_controller.gd
+++ brewing_player_controller.gd
@@ -33,8 +33,7 @@ func _input(event):
 	if event.is_action_pressed("click"):
 		# NOTE: Just the structure of this item
 		## { "position": (-4.876412, 3.20117, 5.886989), "normal": (1.0, 0.0, 0.0), "face_index": -1, "collider_id": 30400316950, "collider": Pot:<StaticBody3D#30400316950>, "shape": 0, "rid": RID(631360192512) }
-		var collided_obj = check_raycast_collision()
-		print(collided_obj)
+		var collided_obj = check_raycast_collision() if not held_item_static_body else check_raycast_collision([held_item_static_body])
 		if !collided_obj.is_empty():
 			# TODO: Ideally the objects themselves only have visual updates, e.g. pot.start_boiling, and everything else handled otherwise
 			# 		or through property access, e.g. cup.tea_type
@@ -51,6 +50,7 @@ var item_interact_funcs = {
 func trigger_item_interaction(item_node_collider):
 	var item_node = item_node_collider.get_parent()
 	var item_name = item_node.name
+	print("Triggering collision for ", item_name)
 	var min_name = item_name.rstrip("0123456789")
 	# TODO: This passes the collider, so get the parent
 	item_interact_funcs[min_name].call(item_node)
@@ -60,6 +60,7 @@ func trigger_item_interaction(item_node_collider):
 # But since I'm not worrying about animations or anything now, we're good
 
 var held_item: Node = null
+var held_item_static_body: StaticBody3D = null
 
 var is_holding_cup = false
 var is_cup_empty = true
@@ -70,7 +71,7 @@ func shelf_interaction(_tea_inv_node):
 		teabag = teabag_model.instantiate()
 		teabag.scale = Vector3(teabag.scale.x*.5, teabag.scale.y*.5, teabag.scale.z*.5)
 		add_child(teabag) # TODO: Add to a different child? But I want it to track there... So maybe I do keep it on player?
-		held_item = teabag
+		set_held_item(teabag)
 		has_teabag = true
 		teabag.global_position = get_hold_location_pos()
 		debug_text_label.text = "Teabag picked up"
@@ -82,24 +83,27 @@ func cup_interaction(cup_node):
 		has_teabag = false
 		cup_node.global_position = get_hold_location_pos()
 		cup_node.reparent(self)
-		held_item = cup_node
+		set_held_item(cup_node)
 		is_holding_cup = true
 		debug_text_label.text = "Cup picked up"
 		# TODO: Can put cup down somewhere to pour water into
 
 func counter_interaction(counter_node):
 	if is_holding_cup:
-		var res = check_raycast_collision([held_item])
-		# PICKUP: Place cup at the intersect point
-		# Will likely need to reset orientation of it so it points up, should be fine to do original rotation
-		
-
-	
-	# TODO: If the cup is not empty trigger serve() or something?
+		var res = check_raycast_collision([held_item.get_node("StaticBody3D")])
+		# again, idk position vs global at this point but no questions lol
+		# The cup origin is in the center of the cup, so I could change that
+		held_item.global_position = Vector3(res.position.x, res.position.y + .3, res.position.z)
+		# Manual adjustments until I make a "proper" snap or something
+		held_item.reparent(counter_node) # I should parent this in game_scene? Or at least set a reference for serving?
+		is_holding_cup = false
+		set_held_item(null)
+	# TODO: If the cup is not empty "is_steeped" trigger serve() or something?
 
 ## Util Functions ##
 
 func check_raycast_collision(exceptions = []):
+	print("Exceptions? ", exceptions)
 	var origin = cam.project_ray_origin($Control.cursor_pos)
 	var end = origin + cam.project_ray_normal($Control.cursor_pos) * ray_length
 	var query = PhysicsRayQueryParameters3D.create(origin, end)
@@ -115,3 +119,14 @@ func get_hold_location_pos():
 	var origin = cam.project_ray_origin($Control.cursor_pos)
 	var end = origin + cam.project_ray_normal($Control.cursor_pos) * 4
 	return end
+
+func set_held_item(held_item_node):
+	if held_item_node != null:
+		held_item = held_item_node
+		if held_item_node.has_node("StaticBody3D"):
+			held_item_static_body = held_item_node.get_node("StaticBody3D")
+		else:
+			held_item_static_body = null
+	else:
+		held_item = null
+		held_item_static_body = null
