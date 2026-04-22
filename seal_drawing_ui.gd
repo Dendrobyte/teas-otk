2630bfd883c27c52767eb1f8e73736749a97696c
Dendrobyte <markbacon78@gmail.com>
2026-04-22 08:43:22 -0700

    Last touches to seal drawing

--- seal_drawing_ui.gd
+++ seal_drawing_ui.gd
@@ -1,8 +1,25 @@
 extends Control
 
 @onready var label = $ResultLabel
+# For now, debugging
+# TODO: We can eventually show a user available seals and their images like constellations / guardian signs
+@onready var seal_menu = $MenuButton
+@onready var clear_button = $ClearButton
 @onready var canvas_bounds = Rect2(Vector2.ZERO, size)
 var _label_base_text = "Result: "
+var all_seals = SealTemplates.ALL_SEALS
+
+func _ready():
+	var seal_popup = seal_menu.get_popup()
+	# This override is one of the few things I'd want to do in the editor, but the popup menu is internal lmao
+	seal_popup.add_theme_font_size_override("font_size", 32)
+
+	for seal_name in all_seals:	
+		seal_popup.add_item(seal_name)
+
+	seal_popup.id_pressed.connect(_show_symbol)
+
+	clear_button.pressed.connect(_clear_canvas)
 
 # STUB: I presume we'll be updating this and iterating over of what to draw
 var drawing_data = {}
@@ -51,3 +68,14 @@ func identify_symbol():
 	var recognized_seal = OneDollar.recognize(normalized_points)
 	actual_seal = recognized_seal.name # DEBUG LINE
 	return recognized_seal
+
+func _show_symbol(menu_idx):
+	var menu_label = seal_menu.get_popup().get_item_text(menu_idx)
+	actual_seal = menu_label
+	queue_redraw()
+
+func _clear_canvas():
+	current_stroke.clear()
+	result_stroke.clear()
+	actual_seal = ""
+	queue_redraw()
