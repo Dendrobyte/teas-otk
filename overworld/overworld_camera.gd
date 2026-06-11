2f3e87c3e9f50d1f38a088c5cae7a963be48e173
Dendrobyte <markbacon78@gmail.com>
2026-06-10 22:36:29 -0700

    Camera futzing

--- overworld/overworld_camera.gd
+++ overworld/overworld_camera.gd
@@ -19,26 +19,34 @@ func _ready():
 	# Save offset from the character and use as our "0 position" to always fix toward
 	char_pos = character.global_position
 	original_offset = Vector3(global_position.x-char_pos.x, global_position.y-char_pos.y, global_position.z-char_pos.z)
+	current_offset = original_offset
 
 var move_bounds = 0.5
 func _physics_process(_delta):
-	# TODO: Don't move if collide
 	# TODO: Working with tethers or some kind of bounds
-	var colliding = false
+	char_pos = character.global_position
+	var move_to = char_pos + original_offset
+	var colliding = _is_going_to_collide(move_to)
 
-	# If the camera can't move, don't do anything
+	# If the camera can't move, update the offset so when we DO move the camera doesn't get off-center
 	# When we can/should move, check where we are in terms of offset
 	# 	If the current_offset is +/- 0.5 within the original_offset, move camera with player
 	# 	Otherwise, update the current_offset based on the character movement (to zero)
-	if not colliding:
+	if colliding:
+		current_offset = global_position - char_pos
+	else:
 		# If we can't move, start to adjust the offset based on the character's position and original offset
-		char_pos = character.global_position
-		current_offset = Vector3(global_position.x-char_pos.x, global_position.y-char_pos.y, global_position.z-char_pos.z)
+		# current_offset = global_position - char_pos
 		var diff = current_offset - original_offset.abs()
-		if diff.x < move_bounds and diff.z < move_bounds:
+		if abs(diff.x) < move_bounds and abs(diff.z) < move_bounds:
 			_update_camera() # Hopefully this isn't too jittery when it resets?
 
 func _update_camera():
-	print("Updating!")
-	# PICKUP HERE
-	global_position = Vector3(global_position.x-char_pos.x, global_position.y-char_pos.y, global_position.z-char_pos.z)
+	global_position = Vector3(char_pos.x+current_offset.x, char_pos.y+current_offset.y, char_pos.z+current_offset.z)
+
+func _is_going_to_collide(target):
+	var space = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(global_position, target)
+	query.exclude = [character.get_rid()]
+	var result = space.intersect_ray(query)
+	return not result.is_empty()
